c4bc7abae85a37c42aa663c0e8687bfd8d493b8f
Ioana Badina <ioana.badina@yahoo.com>
2022-08-22 19:43:06 +0300

    create a map for the Tic Tac Toe game

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
-        <View style={styles.circle}/>
-         
+        <View style={styles.map}>
+          <View style={styles.circle} />
 
-        <View>
-          <View style={styles.crossLine} />
-          <View style={[styles.crossLine, styles.crossLineReversed]} />
+          <View style={styles.cross}>
+            <View style={styles.crossLine} />
+            <View style={[styles.crossLine, styles.crossLineReversed]} />
+          </View>
         </View>
       </ImageBackground>
       <StatusBar style="auto" />
@@ -35,35 +36,49 @@ const styles = StyleSheet.create({
 
     paddingTop: 15,
   },
+  map:{
+    borderWidth:1,
+    borderColor:'white',
+    width:'80%',
+    aspectRatio:1
+  },
   circle: {
+    position: "absolute",
+    left:2*105,
+    top:2*105,
     width: 75,
     height: 75,
     borderRadius: 50,
     alignItems: "center",
     justifyContent: "center",
     margin: 10,
-    borderWidth:10,
-    borderColor:'white'
+    borderWidth: 10,
+    borderColor: "white",
+  },
+  cross:{
+    position: "absolute",
+    width: 75,
+    height: 75,
   },
 
   crossLine: {
-    position:'absolute',
+    position: "absolute",
+    left:32.5,
     width: 10,
     height: 70,
     backgroundColor: "white",
-    borderRadius:5,
+    borderRadius: 5,
     transform: [
       {
-        rotate: '45deg',
+        rotate: "45deg",
       },
     ],
-   
   },
   crossLineReversed: {
     transform: [
       {
-        rotate: '-45deg',
+        rotate: "-45deg",
       },
     ],
-  }
+  },
 });
